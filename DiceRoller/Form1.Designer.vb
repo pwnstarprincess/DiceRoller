366b21819100c5706dc0d4027fb0b1b9b170b099
Jennifer Pezzulo <jennifer.pezzulo@gmail.com>
2020-02-28 18:22:01 -0700

    Add project files.

---
+++ DiceRoller/Form1.Designer.vb
@@ -0,0 +1,279 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.fourSide = New System.Windows.Forms.Button()
+        Me.sixSide = New System.Windows.Forms.Button()
+        Me.eightSided = New System.Windows.Forms.Button()
+        Me.tenSided = New System.Windows.Forms.Button()
+        Me.twelveSided = New System.Windows.Forms.Button()
+        Me.twentySided = New System.Windows.Forms.Button()
+        Me.fourResults = New System.Windows.Forms.TextBox()
+        Me.sixResults = New System.Windows.Forms.TextBox()
+        Me.eightResults = New System.Windows.Forms.TextBox()
+        Me.tenResults = New System.Windows.Forms.TextBox()
+        Me.twelveResults = New System.Windows.Forms.TextBox()
+        Me.twentyResults = New System.Windows.Forms.TextBox()
+        Me.RollEverything = New System.Windows.Forms.Button()
+        Me.ClearEverything = New System.Windows.Forms.Button()
+        Me.Picture4 = New System.Windows.Forms.PictureBox()
+        Me.Picture6 = New System.Windows.Forms.PictureBox()
+        Me.Picture8 = New System.Windows.Forms.PictureBox()
+        Me.Picture10 = New System.Windows.Forms.PictureBox()
+        Me.Picture12 = New System.Windows.Forms.PictureBox()
+        Me.Picture20 = New System.Windows.Forms.PictureBox()
+        CType(Me.Picture4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Picture6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Picture8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Picture10, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Picture12, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Picture20, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'fourSide
+        '
+        Me.fourSide.Location = New System.Drawing.Point(12, 14)
+        Me.fourSide.Name = "fourSide"
+        Me.fourSide.Size = New System.Drawing.Size(75, 35)
+        Me.fourSide.TabIndex = 0
+        Me.fourSide.Text = "4-Sided"
+        Me.fourSide.UseVisualStyleBackColor = True
+        '
+        'sixSide
+        '
+        Me.sixSide.Location = New System.Drawing.Point(331, 14)
+        Me.sixSide.Name = "sixSide"
+        Me.sixSide.Size = New System.Drawing.Size(75, 35)
+        Me.sixSide.TabIndex = 1
+        Me.sixSide.Text = "6-Sided"
+        Me.sixSide.UseVisualStyleBackColor = True
+        '
+        'eightSided
+        '
+        Me.eightSided.Location = New System.Drawing.Point(644, 14)
+        Me.eightSided.Name = "eightSided"
+        Me.eightSided.Size = New System.Drawing.Size(75, 35)
+        Me.eightSided.TabIndex = 2
+        Me.eightSided.Text = "8-Sided"
+        Me.eightSided.UseVisualStyleBackColor = True
+        '
+        'tenSided
+        '
+        Me.tenSided.Location = New System.Drawing.Point(12, 359)
+        Me.tenSided.Name = "tenSided"
+        Me.tenSided.Size = New System.Drawing.Size(100, 35)
+        Me.tenSided.TabIndex = 3
+        Me.tenSided.Text = "10-Sided"
+        Me.tenSided.UseVisualStyleBackColor = True
+        '
+        'twelveSided
+        '
+        Me.twelveSided.Location = New System.Drawing.Point(298, 365)
+        Me.twelveSided.Name = "twelveSided"
+        Me.twelveSided.Size = New System.Drawing.Size(94, 35)
+        Me.twelveSided.TabIndex = 4
+        Me.twelveSided.Text = "12-Sided"
+        Me.twelveSided.UseVisualStyleBackColor = True
+        '
+        'twentySided
+        '
+        Me.twentySided.Location = New System.Drawing.Point(616, 359)
+        Me.twentySided.Name = "twentySided"
+        Me.twentySided.Size = New System.Drawing.Size(103, 35)
+        Me.twentySided.TabIndex = 5
+        Me.twentySided.Text = "20-Sided"
+        Me.twentySided.UseVisualStyleBackColor = True
+        '
+        'fourResults
+        '
+        Me.fourResults.Location = New System.Drawing.Point(12, 274)
+        Me.fourResults.Name = "fourResults"
+        Me.fourResults.ReadOnly = True
+        Me.fourResults.Size = New System.Drawing.Size(162, 26)
+        Me.fourResults.TabIndex = 6
+        '
+        'sixResults
+        '
+        Me.sixResults.Location = New System.Drawing.Point(298, 274)
+        Me.sixResults.Name = "sixResults"
+        Me.sixResults.ReadOnly = True
+        Me.sixResults.Size = New System.Drawing.Size(162, 26)
+        Me.sixResults.TabIndex = 7
+        '
+        'eightResults
+        '
+        Me.eightResults.Location = New System.Drawing.Point(593, 274)
+        Me.eightResults.Name = "eightResults"
+        Me.eightResults.ReadOnly = True
+        Me.eightResults.Size = New System.Drawing.Size(162, 26)
+        Me.eightResults.TabIndex = 8
+        '
+        'tenResults
+        '
+        Me.tenResults.Location = New System.Drawing.Point(12, 636)
+        Me.tenResults.Name = "tenResults"
+        Me.tenResults.ReadOnly = True
+        Me.tenResults.Size = New System.Drawing.Size(162, 26)
+        Me.tenResults.TabIndex = 9
+        '
+        'twelveResults
+        '
+        Me.twelveResults.Location = New System.Drawing.Point(298, 636)
+        Me.twelveResults.Name = "twelveResults"
+        Me.twelveResults.ReadOnly = True
+        Me.twelveResults.Size = New System.Drawing.Size(162, 26)
+        Me.twelveResults.TabIndex = 10
+        '
+        'twentyResults
+        '
+        Me.twentyResults.Location = New System.Drawing.Point(576, 636)
+        Me.twentyResults.Name = "twentyResults"
+        Me.twentyResults.ReadOnly = True
+        Me.twentyResults.Size = New System.Drawing.Size(162, 26)
+        Me.twentyResults.TabIndex = 11
+        '
+        'RollEverything
+        '
+        Me.RollEverything.Location = New System.Drawing.Point(917, 86)
+        Me.RollEverything.Name = "RollEverything"
+        Me.RollEverything.Size = New System.Drawing.Size(174, 46)
+        Me.RollEverything.TabIndex = 12
+        Me.RollEverything.Text = "Roll Everything"
+        Me.RollEverything.UseVisualStyleBackColor = True
+        '
+        'ClearEverything
+        '
+        Me.ClearEverything.Location = New System.Drawing.Point(917, 183)
+        Me.ClearEverything.Name = "ClearEverything"
+        Me.ClearEverything.Size = New System.Drawing.Size(174, 46)
+        Me.ClearEverything.TabIndex = 13
+        Me.ClearEverything.Text = "Clear Everything"
+        Me.ClearEverything.UseVisualStyleBackColor = True
+        '
+        'Picture4
+        '
+        Me.Picture4.Location = New System.Drawing.Point(12, 55)
+        Me.Picture4.Name = "Picture4"
+        Me.Picture4.Size = New System.Drawing.Size(235, 201)
+        Me.Picture4.TabIndex = 14
+        Me.Picture4.TabStop = False
+        '
+        'Picture6
+        '
+        Me.Picture6.Location = New System.Drawing.Point(298, 55)
+        Me.Picture6.Name = "Picture6"
+        Me.Picture6.Size = New System.Drawing.Size(224, 202)
+        Me.Picture6.TabIndex = 15
+        Me.Picture6.TabStop = False
+        '
+        'Picture8
+        '
+        Me.Picture8.Location = New System.Drawing.Point(593, 55)
+        Me.Picture8.Name = "Picture8"
+        Me.Picture8.Size = New System.Drawing.Size(223, 201)
+        Me.Picture8.TabIndex = 16
+        Me.Picture8.TabStop = False
+        '
+        'Picture10
+        '
+        Me.Picture10.Location = New System.Drawing.Point(12, 419)
+        Me.Picture10.Name = "Picture10"
+        Me.Picture10.Size = New System.Drawing.Size(235, 198)
+        Me.Picture10.TabIndex = 17
+        Me.Picture10.TabStop = False
+        '
+        'Picture12
+        '
+        Me.Picture12.Location = New System.Drawing.Point(298, 419)
+        Me.Picture12.Name = "Picture12"
+        Me.Picture12.Size = New System.Drawing.Size(236, 198)
+        Me.Picture12.TabIndex = 18
+        Me.Picture12.TabStop = False
+        '
+        'Picture20
+        '
+        Me.Picture20.Location = New System.Drawing.Point(576, 419)
+        Me.Picture20.Name = "Picture20"
+        Me.Picture20.Size = New System.Drawing.Size(249, 198)
+        Me.Picture20.TabIndex = 19
+        Me.Picture20.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1165, 711)
+        Me.Controls.Add(Me.Picture20)
+        Me.Controls.Add(Me.Picture12)
+        Me.Controls.Add(Me.Picture10)
+        Me.Controls.Add(Me.Picture8)
+        Me.Controls.Add(Me.Picture6)
+        Me.Controls.Add(Me.Picture4)
+        Me.Controls.Add(Me.ClearEverything)
+        Me.Controls.Add(Me.RollEverything)
+        Me.Controls.Add(Me.twentyResults)
+        Me.Controls.Add(Me.twelveResults)
+        Me.Controls.Add(Me.tenResults)
+        Me.Controls.Add(Me.eightResults)
+        Me.Controls.Add(Me.sixResults)
+        Me.Controls.Add(Me.fourResults)
+        Me.Controls.Add(Me.twentySided)
+        Me.Controls.Add(Me.twelveSided)
+        Me.Controls.Add(Me.tenSided)
+        Me.Controls.Add(Me.eightSided)
+        Me.Controls.Add(Me.sixSide)
+        Me.Controls.Add(Me.fourSide)
+        Me.Name = "Form1"
+        Me.Text = "D&D Dice Roller"
+        CType(Me.Picture4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Picture6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Picture8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Picture10, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Picture12, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Picture20, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents fourSide As Button
+    Friend WithEvents sixSide As Button
+    Friend WithEvents eightSided As Button
+    Friend WithEvents tenSided As Button
+    Friend WithEvents twelveSided As Button
+    Friend WithEvents twentySided As Button
+    Friend WithEvents fourResults As TextBox
+    Friend WithEvents sixResults As TextBox
+    Friend WithEvents eightResults As TextBox
+    Friend WithEvents tenResults As TextBox
+    Friend WithEvents twelveResults As TextBox
+    Friend WithEvents twentyResults As TextBox
+    Friend WithEvents RollEverything As Button
+    Friend WithEvents ClearEverything As Button
+    Friend WithEvents Picture4 As PictureBox
+    Friend WithEvents Picture6 As PictureBox
+    Friend WithEvents Picture8 As PictureBox
+    Friend WithEvents Picture10 As PictureBox
+    Friend WithEvents Picture12 As PictureBox
+    Friend WithEvents Picture20 As PictureBox
+End Class
---
+++ DiceRoller/Form1.vb
@@ -0,0 +1,112 @@
+﻿Public Class Form1
+
+
+    ''Shared ReadOnly Generator As System.Random = New System.Random()
+    Private Function RandomNumber(lowerBound As Integer, upperBound As Integer) As Integer
+
+        Static Generator As Random = New Random()
+        Return Generator.Next(lowerBound, upperBound + 1)
+    End Function
+
+    Private Sub DisplayOutput()
+
+    End Sub
+
+    Private Sub FourSide_Click(sender As Object, e As EventArgs) Handles fourSide.Click
+
+        fourResults.Clear()
+        Dim diceRoll As Integer
+        diceRoll = RandomNumber(1, 4)
+        fourResults.Text = diceRoll
+        Picture4.Image = DirectCast(My.Resources.ResourceManager.GetObject("Number" & diceRoll), Image)
+
+    End Sub
+
+    Private Sub SixSide_Click(sender As Object, e As EventArgs) Handles sixSide.Click
+
+
+        sixResults.Clear()
+        Dim diceRoll As Integer
+        diceRoll = RandomNumber(1, 6)
+        sixResults.Text = diceRoll
+        Picture6.Image = DirectCast(My.Resources.ResourceManager.GetObject("Number" & diceRoll), Image)
+
+    End Sub
+
+    Private Sub EightSided_Click(sender As Object, e As EventArgs) Handles eightSided.Click
+
+
+        eightResults.Clear()
+        Dim diceRoll As Integer
+        diceRoll = RandomNumber(1, 8)
+        eightResults.Text = diceRoll
+        Picture8.Image = DirectCast(My.Resources.ResourceManager.GetObject("Number" & diceRoll), Image)
+
+    End Sub
+
+    Private Sub TenSided_Click(sender As Object, e As EventArgs) Handles tenSided.Click
+
+
+        tenResults.Clear()
+        Dim diceRoll As Integer
+        diceRoll = RandomNumber(1, 10)
+        tenResults.Text = diceRoll
+        Picture10.Image = DirectCast(My.Resources.ResourceManager.GetObject("Number" & diceRoll), Image)
+
+    End Sub
+
+    Private Sub TwelveSided_Click(sender As Object, e As EventArgs) Handles twelveSided.Click
+
+
+        twelveResults.Clear()
+        Dim diceRoll As Integer
+        diceRoll = RandomNumber(1, 12)
+        twelveResults.Text = diceRoll
+        Picture12.Image = DirectCast(My.Resources.ResourceManager.GetObject("Number" & diceRoll), Image)
+
+    End Sub
+
+    Private Sub TwentySided_Click(sender As Object, e As EventArgs) Handles twentySided.Click
+
+
+        twentyResults.Clear()
+        Dim diceRoll As Integer
+        diceRoll = RandomNumber(1, 20)
+        twentyResults.Text = diceRoll
+        Picture20.Image = DirectCast(My.Resources.ResourceManager.GetObject("Number" & diceRoll), Image)
+
+    End Sub
+
+    Private Sub RollEverything_Click(sender As Object, e As EventArgs) Handles RollEverything.Click
+        fourResults.Text = RandomNumber(1, 4)
+        Picture4.Image = DirectCast(My.Resources.ResourceManager.GetObject("Number" & fourResults.Text), Image)
+        sixResults.Text = RandomNumber(1, 6)
+        Picture6.Image = DirectCast(My.Resources.ResourceManager.GetObject("Number" & sixResults.Text), Image)
+        eightResults.Text = RandomNumber(1, 8)
+        Picture8.Image = DirectCast(My.Resources.ResourceManager.GetObject("Number" & eightResults.Text), Image)
+        tenResults.Text = RandomNumber(1, 10)
+        Picture10.Image = DirectCast(My.Resources.ResourceManager.GetObject("Number" & tenResults.Text), Image)
+        twelveResults.Text = RandomNumber(1, 12)
+        Picture12.Image = DirectCast(My.Resources.ResourceManager.GetObject("Number" & twelveResults.Text), Image)
+        twentyResults.Text = RandomNumber(1, 20)
+        Picture20.Image = DirectCast(My.Resources.ResourceManager.GetObject("Number" & twentyResults.Text), Image)
+
+    End Sub
+
+    Private Sub ClearEverything_Click(sender As Object, e As EventArgs) Handles ClearEverything.Click
+        fourResults.Clear()
+        Picture4.Image = Nothing
+        sixResults.Clear()
+        Picture6.Image = Nothing
+        eightResults.Clear()
+        Picture8.Image = Nothing
+        tenResults.Clear()
+        Picture10.Image = Nothing
+        twelveResults.Clear()
+        Picture12.Image = Nothing
+        twentyResults.Clear()
+        Picture20.Image = Nothing
+    End Sub
+
+
+End Class
